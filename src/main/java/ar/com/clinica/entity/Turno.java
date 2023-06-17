027c3bc2063af48345bbdbe4618a37fe332c0057
eligazza <gazzaelias@gmail.com>
2023-06-16 23:39:42 -0300

    atributos fecha y hora

--- src/main/java/ar/com/clinica/entity/Turno.java
+++ src/main/java/ar/com/clinica/entity/Turno.java
@@ -1,9 +1,15 @@
 package ar.com.clinica.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 @Entity
 @Table (name = "turnos")
@@ -12,7 +18,8 @@ public class Turno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String fecha;
+    private LocalDate fecha;
+    private LocalDate hora;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "odontologo_id", referencedColumnName = "id", nullable = false)
@@ -22,11 +29,4 @@ public class Turno {
     @JoinColumn(name = "paciente_id", referencedColumnName = "id", nullable = false)
     private Paciente paciente;
 
-
-    public Turno(){}
-    public Turno(String fecha, Odontologo odontologo, Paciente paciente) {
-        this.fecha = fecha;
-        this.odontologo = odontologo;
-        this.paciente = paciente;
-    }
 }
