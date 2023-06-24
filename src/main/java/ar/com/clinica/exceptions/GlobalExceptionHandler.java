3a0cf532c95dad53cbdf1f0ed53eb17a9c0230d8
eligazza <gazzaelias@gmail.com>
2023-06-23 23:31:22 -0300

    exception improvement

--- src/main/java/ar/com/clinica/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ar/com/clinica/exceptions/GlobalExceptionHandler.java
@@ -1,25 +1,37 @@
 package ar.com.clinica.exceptions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalException {
+public class GlobalExceptionHandler {
+
+
+    private static final Logger LOG = LogManager.getLogger(GlobalExceptionHandler.class);
+
 
     @ExceptionHandler({ExcepcionNoHayContenido.class})
-    public ResponseEntity<String> procesarNoHayContenido(ExcepcionNoHayContenido ex) {
+    public ResponseEntity<String> manejarNoHayContenido(ExcepcionNoHayContenido ex) {
+        ex.printStackTrace();
+        LOG.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler({ExcepcionRecursoNoEncontrado.class})
-    public ResponseEntity<String> procesarRecursoNoEncontrado(ExcepcionRecursoNoEncontrado ex) {
+    public ResponseEntity<String> manejarRecursoNoEncontrado(ExcepcionRecursoNoEncontrado ex) {
+        ex.printStackTrace();
+        LOG.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler({ExcepcionParametroFaltante.class})
-    public ResponseEntity<String> procesarParametroFaltante(ExcepcionParametroFaltante ex) {
+    public ResponseEntity<String> manejarParametroFaltante(ExcepcionParametroFaltante ex) {
+        ex.printStackTrace();
+        LOG.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
