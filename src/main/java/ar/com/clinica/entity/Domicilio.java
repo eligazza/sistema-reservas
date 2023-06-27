9b40183002c4883825b9b07d4225ca34f75ec3cb
eligazza <gazzaelias@gmail.com>
2023-06-27 02:32:55 -0300

    fixed: constructors not working

--- src/main/java/ar/com/clinica/entity/Domicilio.java
+++ src/main/java/ar/com/clinica/entity/Domicilio.java
@@ -1,10 +1,14 @@
 package ar.com.clinica.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "domicilios")
 public class Domicilio {
 
