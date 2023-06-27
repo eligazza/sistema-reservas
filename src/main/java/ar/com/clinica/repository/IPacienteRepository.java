1755eef571b3bb424be03ad48583c0c860bd6e37
eligazza <gazzaelias@gmail.com>
2023-06-27 02:26:55 -0300

    Añado validacion de duplicados en pacientes

--- src/main/java/ar/com/clinica/repository/IPacienteRepository.java
+++ src/main/java/ar/com/clinica/repository/IPacienteRepository.java
@@ -4,6 +4,10 @@ import ar.com.clinica.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IPacienteRepository extends JpaRepository<Paciente, Long> {
+
+    Optional<Paciente> findByDni(Integer dni);
 }
