9fe75b63fc933ab6c8d9357a787e74c3bd0c5df2
eligazza <gazzaelias@gmail.com>
2023-06-28 22:36:06 -0300

    Cambio "datetime" por "date" en las fecha-de-alta de Paciente

--- src/main/java/ar/com/clinica/dto/response/PacienteDtoResponse.java
+++ src/main/java/ar/com/clinica/dto/response/PacienteDtoResponse.java
@@ -16,7 +16,7 @@ public class PacienteDtoResponse {
     private Integer dni;
     private String nombre;
     private String apellido;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date fechaDeAlta;
     private DomicilioDtoResponse domicilio;
 
--- src/main/java/ar/com/clinica/entity/Paciente.java
+++ src/main/java/ar/com/clinica/entity/Paciente.java
@@ -21,7 +21,7 @@ public class Paciente {
     private Integer dni;
     private String nombre;
     private String apellido;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date fechaDeAlta;
 
     @OneToOne(cascade = CascadeType.ALL)
