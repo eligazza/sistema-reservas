5403e585f388407cdad8180f0d4c9a1ff87a8b89
eligazza <gazzaelias@gmail.com>
2023-07-03 16:41:13 -0300

    Añado Logging para Errores

--- src/main/java/ar/com/clinica/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ar/com/clinica/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package ar.com.clinica.exceptions;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
